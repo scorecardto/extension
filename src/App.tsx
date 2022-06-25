cd671c2f8582f81a1439c4b8e176cc72b6f3f93b
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-06-24 20:18:40 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/main/Main";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,5 @@
+function Main() {
+  return <div>main section</div>;
+}
+
+export default Main;
