9afd62fa6cde1f4891a66f5827d7b66434d9b3dc
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-09-30 09:58:43 -0500

    add grade spoilers

--- page/components/main/CourseGrade.tsx
+++ page/components/main/CourseGrade.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Chip from "./Chip";
 
 function CourseGrade(props: { courseName: string; grade: string }) {
@@ -5,8 +6,10 @@ function CourseGrade(props: { courseName: string; grade: string }) {
     <div className="flex justify-between items-center border-b last:border-b-0 border-b-mono-200 py-2 px-4">
       <p>{props.courseName}</p>
 
-      <div className="from-accent-500 to-accent-600 bg-gradient-to-tr w-12 text-center py-1.5 rounded-full text-xs text-white">
-        {props.grade}
+      <div className="from-accent-500 to-accent-600 bg-gradient-to-tr w-12 text-center py-1.5 rounded-full group">
+        <p className="opacity-20 group-hover:opacity-100 transition-opacity text-xs text-white">
+          {props.grade}
+        </p>
       </div>
     </div>
   );
