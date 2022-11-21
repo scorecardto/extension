ac8af90e66e26566213b7c35a72e850c32e5db90
MaliciousFiles <mtroalson@gmail.com>
2022-11-20 18:02:25 -0600

    clicking on a course takes you to the webpage

--- page/components/main/CourseGrade.tsx
+++ page/components/main/CourseGrade.tsx
@@ -3,7 +3,7 @@ import Chip from "./Chip";
 
 function CourseGrade(props: { courseName: string; grade: string }) {
   return (
-    <div className="group flex justify-between items-center border-b last:border-b-0 border-b-mono-200 py-2 px-4 hover:bg-mono-150 cursor-pointer overflow-hidden">
+    <div onClick={() => {chrome.tabs.create({url: "https://scorecard-iota.vercel.app/app#"+props.courseName})}} className="group flex justify-between items-center border-b last:border-b-0 border-b-mono-200 py-2 px-4 hover:bg-mono-150 cursor-pointer overflow-hidden">
       <p>{props.courseName}</p>
 
       <div className="from-accent-500 to-accent-600 bg-gradient-to-tr w-12 text-center py-1.5 rounded-full">
