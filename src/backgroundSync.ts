34a2df11a0da9e91566150d1120f9e9a0678ce8b
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-11-21 13:31:09 -0600

    implement setting for background sync

--- src/backgroundSync.ts
+++ src/backgroundSync.ts
@@ -1,11 +1,9 @@
 import Dexie from "dexie";
 import { fetchAndStoreContent } from "./connector";
 
-const FREQUENCY_MINUTES = 30;
+const DEFAULT_FREQUENCY_MINUTES = 30;
 
 export function startBackgroundSync(db: Dexie) {
-  chrome.alarms.create("fetchGrades", { periodInMinutes: FREQUENCY_MINUTES });
-
   chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "fetchGrades") {
       console.log("Fetching grades in background ...");
@@ -22,6 +20,34 @@ export function startBackgroundSync(db: Dexie) {
           });
         }
       });
+    }
+  });
+  chrome.storage.local.get(["settings"], (res) => {
+    const settings = res["settings"];
+    if (settings?.checkGradesInterval) {
+      chrome.alarms.create("fetchGrades", {
+        periodInMinutes: settings.checkGradesInterval,
+      });
+    } else {
+      chrome.alarms.create("fetchGrades", {
+        periodInMinutes: DEFAULT_FREQUENCY_MINUTES,
+      });
+    }
+  });
+
+  // listen for updates to chrome storage
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === "local" && changes["settings"]) {
+      const settings = changes["settings"].newValue;
+      if (settings?.checkGradesInterval) {
+        chrome.alarms.create("fetchGrades", {
+          periodInMinutes: settings.checkGradesInterval,
+        });
+      } else {
+        chrome.alarms.create("fetchGrades", {
+          periodInMinutes: DEFAULT_FREQUENCY_MINUTES,
+        });
+      }
     }
   });
 }
